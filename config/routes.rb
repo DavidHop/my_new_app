9ed55bd879e3f3f7b299a3d4eaca97677b82f55d
David Hopkins <davidlucashopkins@gmail.com>
2019-01-25 18:53:16 -0500

    5.3 Mailers added

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
+  post 'simple_pages/thank_you'
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
