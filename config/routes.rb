58c59e87896a093411f92fa1f9d2d6531bf48b94
David Hopkins <davidlucashopkins@gmail.com>
2019-01-26 16:45:18 -0500

    5.4 routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
   resources :products
   resources :object_names
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
