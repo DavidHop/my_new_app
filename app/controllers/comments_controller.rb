e59830625dd733c4fcc93f4b6c86b2f8be67248a
David Hopkins <davidlucashopkins@gmail.com>
2019-03-01 10:59:36 -0500

    5.10 added admin role & moderating user comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,6 +14,14 @@ class CommentsController < ApplicationController
       end
     end
   end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
+  end
+  
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
