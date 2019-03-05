a472dc8d2cd5c9ffdd972442ad7b2d3fee6f567b
David Hopkins <davidlucashopkins@gmail.com>
2019-03-04 19:57:47 -0500

    6.6 removed product price, added static price for now

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,7 +10,7 @@ class PaymentsController < ApplicationController
   # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        amount: (@product.price * 100).to_i,
+        amount: 2000, # amount in cents, again
         currency: "usd",
         :source => token,
         :description => params[:stripeEmail],
