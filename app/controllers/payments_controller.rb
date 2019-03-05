dc241d0445146f4fa939712f870746408e28b5e3
David Hopkins <davidlucashopkins@gmail.com>
2019-03-04 19:14:45 -0500

    6.6 test

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,44 +1,68 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
+
+
   def create
     @ordersObject = params[:order_ids]
     @user = current_user
-    # lets loop this and get the total ammount
+    # lets loop this and get the total amount
     @totalPrice = 0
     @ordersObject.each do |order_id|
       order = Order.find(order_id)
 
+
+      if order.user_id != @user.id
+        redirect_to orders_path, alert: "Something went wrong with your order... Refresh the page and try again."
+      end
+
       @totalPrice = @totalPrice + order.total
     end
 
-  token = params[:stripeToken]
-  # Create the charge on Stripe's servers - this will charge the user's card
+
+
+    token = params[:stripeToken]
+
+
     begin
       charge = Stripe::Charge.create(
-        amount: (@product.price * 100).to_i,
-        currency: "usd",
-        :source => token,
-        :description => params[:stripeEmail],
-        :receipt_email => params[:stripeEmail]
+        amount: @totalPrice,
+        currency: "eur",
+        source: token,
+        description: params[:stripeEmail]
       )
 
-      if charge.paid
-        Order.create(
-          product_id: @product.id,
-          user_id: @user.id,
-          total: @product.price
-        )
-        redirect_to orders_path, notice: "Thank you for the payment"
+    if charge.paid
+
+
+
+      @ordersObject.each do |order_id|
+
+        tmpVar = Order.find(order_id)
+
+        if tmpVar.user_id == @user.id
+          tmpVar.paid = 1
+          tmpVar.save
+
+        else
+          redirect_to orders_path, alert: "Your order has been paid but an error ocurred. Please contact the support"
+        end
+
       end
 
-      rescue Stripe::CardError => e
-        # The card has been declined
-        body = e.json_body
-        err = body[:error]
-        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
-      end
-        redirect_to product_path(@product)
+      redirect_to orders_path, notice: "Thank you for the <span class='dollar'>€</span>#{@totalPrice}! Items will never arrive ahahaha."
+
     end
+
+    # Something went wrong lets display what
+    rescue Stripe::CardError => e
+
+      body = e.json_body
+      err = body[:error]
+
+      redirect_to orders_path, alert: "Something went wrong with your payment request.<br><br>ERROR.: #{err[:message]}."
+    end
+
+
 
   end
