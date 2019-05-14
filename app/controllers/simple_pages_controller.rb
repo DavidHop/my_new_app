5b9d5b6c0ca4fdd7043e34e9c30bcf4dd902e080
David Hopkins <davidlucashopkins@gmail.com>
2019-05-14 17:11:19 -0400

    added product view counter

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,7 +1,9 @@
 class SimplePagesController < ApplicationController
 
   def landing_page
-      @featured_product = Product.first
+    @featured_product = Product.first
+    $redis.getset('hit_counter', 0) # finds or creates the hit counter
+    @hit_counter = $redis.incr('hit_counter') # updates the hit counter by 1
   end
 
   def about
